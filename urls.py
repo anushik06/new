ff38842cf8ce8648aac289f4f44d4f5be28b77c1
anushik06 <138489845+anushik06@users.noreply.github.com>
2023-07-30 15:18:03 +0400

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-      path("",views.index, name="home"),
-      path("about",views.about, name="about"),
-      path("course",views.course, name="course")
-]
